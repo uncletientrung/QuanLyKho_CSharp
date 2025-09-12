be188554ba44934aee0e2ef5248b93ae0dcce2bf
uncletientrung <thihachcf@gmail.com>
2025-09-13 00:42:52 +0700

    database chua co hoan hang

--- Database/quanlikhoquanao.sql
+++ Database/quanlikhoquanao.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 12, 2025 at 07:26 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th9 12, 2025 lúc 07:40 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,185 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `quanlikhoquanao`
+-- Cơ sở dữ liệu: `quanlikhoquanao`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nhanvien`
+-- Cấu trúc bảng cho bảng `chatlieu`
+--
+
+CREATE TABLE `chatlieu` (
+  `machatlieu` int(11) NOT NULL,
+  `tenchatlieu` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `ctkiemke`
+--
+
+CREATE TABLE `ctkiemke` (
+  `maphieukiemke` int(11) NOT NULL,
+  `masp` int(11) DEFAULT NULL,
+  `tonchinhanh` int(50) NOT NULL DEFAULT 0,
+  `tonthucthe` int(50) NOT NULL DEFAULT 0,
+  `ghchu` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `lech` int(50) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `ctphieunhap`
+--
+
+CREATE TABLE `ctphieunhap` (
+  `maphieunhap` int(11) NOT NULL,
+  `masp` int(11) DEFAULT NULL,
+  `soluong` int(11) NOT NULL DEFAULT 0,
+  `dongia` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `ctphieuxuat`
+--
+
+CREATE TABLE `ctphieuxuat` (
+  `maphieuxuat` int(11) NOT NULL,
+  `masp` int(11) DEFAULT NULL,
+  `soluong` int(11) NOT NULL DEFAULT 0,
+  `dongia` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `ctquyen`
+--
+
+CREATE TABLE `ctquyen` (
+  `manhomquyen` int(11) NOT NULL,
+  `machucnang` int(11) NOT NULL,
+  `hanhdong` varchar(255) NOT NULL,
+  `trangthai` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `danhmucchucnang`
+--
+
+CREATE TABLE `danhmucchucnang` (
+  `machucnang` int(11) NOT NULL,
+  `tenchucnang` varchar(255) NOT NULL,
+  `trangthai` tinyint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `danhmucchucnang`
+--
+
+INSERT INTO `danhmucchucnang` (`machucnang`, `tenchucnang`, `trangthai`) VALUES
+(1, 'Quản lý nhân viên', 1),
+(2, 'Quản lý khách hàng', 1),
+(3, 'Quản lý nhà cung cấp', 1),
+(4, 'Quản lý nhà xuất bản', 1),
+(5, 'Quản lý tác giả', 1),
+(6, 'Quản lý thể loại', 1),
+(7, 'Quản lý nhóm quyền', 1),
+(8, 'Quản lý tài khoản', 1),
+(9, 'Quản lý nhập hàng', 1),
+(10, 'Quản lý xuất hàng', 1),
+(11, 'Quản lý thống kê', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khachhang`
+--
+
+CREATE TABLE `khachhang` (
+  `makh` int(11) NOT NULL,
+  `hokhachhang` varchar(255) DEFAULT NULL,
+  `tenkhachhang` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `ngaysinh` date DEFAULT NULL,
+  `sdt` varchar(50) DEFAULT NULL,
+  `trangthai` tinyint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `khachhang`
+--
+
+INSERT INTO `khachhang` (`makh`, `hokhachhang`, `tenkhachhang`, `email`, `ngaysinh`, `sdt`, `trangthai`) VALUES
+(1, 'Nguyễn Văn', 'An', 'an.nguyen@example.com', '1996-05-14', '0912345678', 1),
+(2, 'Trần Thị', 'Bình', 'binh.tran@example.com', '1988-10-22', '0987654321', 1),
+(3, 'Lê Minh', 'Cường', 'cuong.le@example.com', '1995-07-30', '0909876543', 1),
+(4, 'Phạm Thảo', 'Dung', 'dung.pham@example.com', '2000-03-15', '0965123456', 1),
+(5, 'Đặng Hữu', 'Đức', 'duc.dang@example.com', '1985-08-05', '0978456123', 1),
+(6, 'Hồ Lan', 'Hương	', 'huong.ho@example.com', '1993-11-25', '0911122233', 1),
+(7, 'Bùi Khánh', 'Hòa', 'hoa.bui@example.com', '1998-06-18', '0922233344', 1),
+(8, 'Võ Hoàng', 'Khang', 'khang.vo@example.com', '1987-09-12', '0933344455', 1),
+(9, 'Nguyễn Mai', 'Linh', 'linh.nguyen@example.com', '1996-04-07', '0944455566', 1),
+(10, 'Trịnh Ngọc', 'Minh', 'minh.trinh@example.com', '2002-02-28', '0955566677', 1),
+(11, 'Dương Thanh', 'Nam', 'nam.duong@example.com', '1990-12-01', '0966677788', 1),
+(12, 'Phan Anh', 'Quân', 'quan.phan@example.com	', '1989-07-19', '0977788899', 1),
+(13, 'Lương Thu', 'Trang', 'trang.luong@example.com', '1997-01-23', '0988899000', 1),
+(14, 'Lương ', 'Thuận', 'thihachcf@gmail.com', '2005-01-01', '0352447642', 1),
+(15, 'Trung', 'Trung', 'thihahcf@gmail.com', '2022-11-11', '0352447642', 1),
+(16, 'Lê ', 'Văn Luyện', 'thi@gmail.com', '2005-12-12', '0987654321', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khuvuckho`
+--
+
+CREATE TABLE `khuvuckho` (
+  `makhuvuc` int(11) NOT NULL,
+  `tenkhuvuc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `diachi` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `sdt` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `loai`
+--
+
+CREATE TABLE `loai` (
+  `maloai` int(11) NOT NULL,
+  `tenloai` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhacungcap`
+--
+
+CREATE TABLE `nhacungcap` (
+  `mancc` int(11) NOT NULL,
+  `tenncc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `diachincc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `sdt` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `trangthai` tinyint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhanvien`
 --
 
 CREATE TABLE `nhanvien` (
@@ -37,31 +209,396 @@ CREATE TABLE `nhanvien` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `nhanvien`
+-- Đang đổ dữ liệu cho bảng `nhanvien`
 --
 
 INSERT INTO `nhanvien` (`manv`, `tennv`, `gioitinh`, `sdt`, `ngaysinh`, `trangthai`) VALUES
 (1, 'Nguyễn Tiến Trung', 1, '0352447642', '2000-03-15', 1);
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Cấu trúc bảng cho bảng `nhomquyen`
+--
+
+CREATE TABLE `nhomquyen` (
+  `manhomquyen` int(11) NOT NULL,
+  `tennhomquyen` varchar(255) NOT NULL,
+  `trangthai` tinyint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `nhomquyen`
+--
+
+INSERT INTO `nhomquyen` (`manhomquyen`, `tennhomquyen`, `trangthai`) VALUES
+(1, 'Quản lý', 1),
+(2, 'Nhân viên', 1),
+(3, 'Quản lý kho', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phieukiemke`
+--
+
+CREATE TABLE `phieukiemke` (
+  `maphieukiemke` int(11) NOT NULL,
+  `thoigiantao` datetime NOT NULL DEFAULT current_timestamp(),
+  `thoigiancanbang` datetime NOT NULL DEFAULT current_timestamp(),
+  `trangthai` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `ghichu` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `makhuvuc` int(11) DEFAULT NULL,
+  `nhanvientao` int(11) DEFAULT NULL,
+  `nhanvienkiem` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phieunhap`
+--
+
+CREATE TABLE `phieunhap` (
+  `maphieunhap` int(11) NOT NULL,
+  `manv` int(11) DEFAULT NULL,
+  `mancc` int(11) DEFAULT NULL,
+  `thoigiantao` datetime DEFAULT current_timestamp(),
+  `tongtien` bigint(20) NOT NULL DEFAULT 0,
+  `trangthai` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phieuxuat`
+--
+
+CREATE TABLE `phieuxuat` (
+  `maphieuxuat` int(11) NOT NULL,
+  `manv` int(11) DEFAULT NULL,
+  `makh` int(11) DEFAULT NULL,
+  `thoigiantao` datetime NOT NULL DEFAULT current_timestamp(),
+  `tongtien` bigint(20) DEFAULT 0,
+  `trangthai` tinyint(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `masp` int(11) NOT NULL,
+  `tensp` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `hinhanh` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `soluong` int(11) DEFAULT 0,
+  `dongia` int(50) DEFAULT 0,
+  `machatlieu` int(11) DEFAULT NULL,
+  `maloai` int(11) DEFAULT NULL,
+  `makhuvuc` int(11) DEFAULT NULL,
+  `masize` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `size`
+--
+
+CREATE TABLE `size` (
+  `masize` int(11) NOT NULL,
+  `tensize` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `ghi` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `taikhoan`
+--
+
+CREATE TABLE `taikhoan` (
+  `manv` int(11) NOT NULL,
+  `tendangnhap` varchar(255) NOT NULL,
+  `matkhau` varchar(255) NOT NULL,
+  `manhomquyen` int(11) NOT NULL,
+  `trangthai` tinyint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `taikhoan`
+--
+
+INSERT INTO `taikhoan` (`manv`, `tendangnhap`, `matkhau`, `manhomquyen`, `trangthai`) VALUES
+(1, 'trung123', '12345', 3, 1),
+(6, 'admin', '12345', 1, 1);
+
+--
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `nhanvien`
+-- Chỉ mục cho bảng `chatlieu`
+--
+ALTER TABLE `chatlieu`
+  ADD PRIMARY KEY (`machatlieu`);
+
+--
+-- Chỉ mục cho bảng `ctkiemke`
+--
+ALTER TABLE `ctkiemke`
+  ADD KEY `FK_ctkiemke_phieukiemke` (`maphieukiemke`),
+  ADD KEY `FK_ctkiemke_sanpham` (`masp`);
+
+--
+-- Chỉ mục cho bảng `ctphieunhap`
+--
+ALTER TABLE `ctphieunhap`
+  ADD KEY `FK_ctphieunhap_phieunhap` (`maphieunhap`),
+  ADD KEY `FK_ctphieunhap_sanpham` (`masp`);
+
+--
+-- Chỉ mục cho bảng `ctphieuxuat`
+--
+ALTER TABLE `ctphieuxuat`
+  ADD KEY `FK_ctphieuxuat_sanpham` (`masp`),
+  ADD KEY `FK_ctphieuxuat_phieuxuat` (`maphieuxuat`);
+
+--
+-- Chỉ mục cho bảng `ctquyen`
+--
+ALTER TABLE `ctquyen`
+  ADD PRIMARY KEY (`manhomquyen`,`machucnang`,`hanhdong`);
+
+--
+-- Chỉ mục cho bảng `danhmucchucnang`
+--
+ALTER TABLE `danhmucchucnang`
+  ADD PRIMARY KEY (`machucnang`);
+
+--
+-- Chỉ mục cho bảng `khachhang`
+--
+ALTER TABLE `khachhang`
+  ADD PRIMARY KEY (`makh`);
+
+--
+-- Chỉ mục cho bảng `khuvuckho`
+--
+ALTER TABLE `khuvuckho`
+  ADD PRIMARY KEY (`makhuvuc`);
+
+--
+-- Chỉ mục cho bảng `loai`
+--
+ALTER TABLE `loai`
+  ADD PRIMARY KEY (`maloai`);
+
+--
+-- Chỉ mục cho bảng `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  ADD PRIMARY KEY (`mancc`);
+
+--
+-- Chỉ mục cho bảng `nhanvien`
 --
 ALTER TABLE `nhanvien`
   ADD PRIMARY KEY (`manv`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Chỉ mục cho bảng `nhomquyen`
+--
+ALTER TABLE `nhomquyen`
+  ADD PRIMARY KEY (`manhomquyen`);
+
+--
+-- Chỉ mục cho bảng `phieukiemke`
+--
+ALTER TABLE `phieukiemke`
+  ADD PRIMARY KEY (`maphieukiemke`),
+  ADD KEY `FK_phieukiemke_khuvuc` (`makhuvuc`),
+  ADD KEY `FK_phieukiemke_nhanvien` (`nhanvienkiem`),
+  ADD KEY `FK_phieukiemke_taikhoan` (`nhanvientao`);
+
+--
+-- Chỉ mục cho bảng `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  ADD PRIMARY KEY (`maphieunhap`),
+  ADD KEY `FK_phieunhap_nhacungcap` (`mancc`),
+  ADD KEY `FK_phieunhap_taikhoan` (`manv`);
+
+--
+-- Chỉ mục cho bảng `phieuxuat`
+--
+ALTER TABLE `phieuxuat`
+  ADD PRIMARY KEY (`maphieuxuat`),
+  ADD KEY `FK_phieuxuat_khachhang` (`makh`),
+  ADD KEY `FK_phieuxuat_taikhoan` (`manv`);
+
+--
+-- Chỉ mục cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`masp`),
+  ADD KEY `FK_sanpham_chatlieu` (`machatlieu`),
+  ADD KEY `FK_sanpham_loai` (`maloai`),
+  ADD KEY `FK_sanpham_size` (`masize`),
+  ADD KEY `FK_sanpham_khucvuc` (`makhuvuc`);
+
+--
+-- Chỉ mục cho bảng `size`
+--
+ALTER TABLE `size`
+  ADD PRIMARY KEY (`masize`);
+
+--
+-- Chỉ mục cho bảng `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD PRIMARY KEY (`manv`),
+  ADD KEY `FK_taikhoan_nhomquyen` (`manhomquyen`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `nhanvien`
+-- AUTO_INCREMENT cho bảng `chatlieu`
+--
+ALTER TABLE `chatlieu`
+  MODIFY `machatlieu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `danhmucchucnang`
+--
+ALTER TABLE `danhmucchucnang`
+  MODIFY `machucnang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT cho bảng `khachhang`
+--
+ALTER TABLE `khachhang`
+  MODIFY `makh` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT cho bảng `khuvuckho`
+--
+ALTER TABLE `khuvuckho`
+  MODIFY `makhuvuc` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `loai`
+--
+ALTER TABLE `loai`
+  MODIFY `maloai` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  MODIFY `mancc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT cho bảng `nhanvien`
 --
 ALTER TABLE `nhanvien`
   MODIFY `manv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `nhomquyen`
+--
+ALTER TABLE `nhomquyen`
+  MODIFY `manhomquyen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `phieukiemke`
+--
+ALTER TABLE `phieukiemke`
+  MODIFY `maphieukiemke` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  MODIFY `maphieunhap` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT cho bảng `phieuxuat`
+--
+ALTER TABLE `phieuxuat`
+  MODIFY `maphieuxuat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=199;
+
+--
+-- AUTO_INCREMENT cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `masp` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `size`
+--
+ALTER TABLE `size`
+  MODIFY `masize` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `ctkiemke`
+--
+ALTER TABLE `ctkiemke`
+  ADD CONSTRAINT `FK_ctkiemke_phieukiemke` FOREIGN KEY (`maphieukiemke`) REFERENCES `phieukiemke` (`maphieukiemke`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_ctkiemke_sanpham` FOREIGN KEY (`masp`) REFERENCES `sanpham` (`masp`) ON DELETE SET NULL;
+
+--
+-- Các ràng buộc cho bảng `ctphieunhap`
+--
+ALTER TABLE `ctphieunhap`
+  ADD CONSTRAINT `FK_ctphieunhap_phieunhap` FOREIGN KEY (`maphieunhap`) REFERENCES `phieunhap` (`maphieunhap`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_ctphieunhap_sanpham` FOREIGN KEY (`masp`) REFERENCES `sanpham` (`masp`) ON DELETE SET NULL;
+
+--
+-- Các ràng buộc cho bảng `ctphieuxuat`
+--
+ALTER TABLE `ctphieuxuat`
+  ADD CONSTRAINT `FK_ctphieuxuat_phieuxuat` FOREIGN KEY (`maphieuxuat`) REFERENCES `phieuxuat` (`maphieuxuat`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_ctphieuxuat_sanpham` FOREIGN KEY (`masp`) REFERENCES `sanpham` (`masp`) ON DELETE SET NULL;
+
+--
+-- Các ràng buộc cho bảng `phieukiemke`
+--
+ALTER TABLE `phieukiemke`
+  ADD CONSTRAINT `FK_phieukiemke_khuvuc` FOREIGN KEY (`makhuvuc`) REFERENCES `khuvuckho` (`makhuvuc`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_phieukiemke_nhanvien` FOREIGN KEY (`nhanvienkiem`) REFERENCES `nhanvien` (`manv`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_phieukiemke_taikhoan` FOREIGN KEY (`nhanvientao`) REFERENCES `taikhoan` (`manv`) ON DELETE SET NULL;
+
+--
+-- Các ràng buộc cho bảng `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  ADD CONSTRAINT `FK_phieunhap_nhacungcap` FOREIGN KEY (`mancc`) REFERENCES `nhacungcap` (`mancc`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_phieunhap_taikhoan` FOREIGN KEY (`manv`) REFERENCES `taikhoan` (`manv`) ON DELETE SET NULL;
+
+--
+-- Các ràng buộc cho bảng `phieuxuat`
+--
+ALTER TABLE `phieuxuat`
+  ADD CONSTRAINT `FK_phieuxuat_khachhang` FOREIGN KEY (`makh`) REFERENCES `khachhang` (`makh`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_phieuxuat_taikhoan` FOREIGN KEY (`manv`) REFERENCES `taikhoan` (`manv`) ON DELETE SET NULL;
+
+--
+-- Các ràng buộc cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD CONSTRAINT `FK_sanpham_chatlieu` FOREIGN KEY (`machatlieu`) REFERENCES `chatlieu` (`machatlieu`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_sanpham_khucvuc` FOREIGN KEY (`makhuvuc`) REFERENCES `khuvuckho` (`makhuvuc`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_sanpham_loai` FOREIGN KEY (`maloai`) REFERENCES `loai` (`maloai`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_sanpham_size` FOREIGN KEY (`masize`) REFERENCES `size` (`masize`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
