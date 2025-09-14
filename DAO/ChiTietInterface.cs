b0e7337a35b7659839c1a3ef7d009cb22b4e4047
uncletientrung <thihachcf@gmail.com>
2025-09-14 14:25:11 +0700

    Chi Tiet Interface

---
+++ DAO/ChiTietInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho_CSharp.DAO
+{
+    public interface ChiTietInterface<T>
+    {
+        int Insert(BindingList<T> list);
+        int Delete(int ma);
+        int Update(BindingList<T> t, int ma);
+        BindingList<T> SelectAll(int ma);
+
+    }
+}
