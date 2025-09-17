409c367dc94f7948e69d6187fe04a872346c2dde
Tung Vu <155795286+Stup1dCate@users.noreply.github.com>
2025-09-17 17:00:54 +0700

    Add files via upload

---
+++ GUI/HoanHang/XacNhanHoanHang.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKho_CSharp.GUI.HoanHang
+{
+    public partial class XacNhanHoanHang : Form
+    {
+        public XacNhanHoanHang()
+        {
+            InitializeComponent();
+        }
+
+        // chỉ đọc
+        private void lbInfo_Click(object sender, EventArgs e) {}
+
+        private void label1_Click(object sender, EventArgs e) {}
+
+        // nút hoàn hàng
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Xử lý xác nhận hoàn hàng ở đây
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void XacNhanHoanHang_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
