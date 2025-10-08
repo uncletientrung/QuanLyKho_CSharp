1306839b8ac3b9e6e4e6966f8f032f1ed197addb
Thanh Tùng <phanthanhtung071105@gmail.com>
2025-10-08 21:17:06 +0700

    done detail

--- GUI/ThongTin/ChatLieu/DetailChatLieuForm.cs
+++ GUI/ThongTin/ChatLieu/DetailChatLieuForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyKho_CSharp.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
 
 namespace QuanLyKho_CSharp.GUI.ThongTin.ChatLieu
 {
     public partial class DetailChatLieuForm : Form
     {
-        public DetailChatLieuForm()
+        ChatLieuDTO cl;
+
+        public DetailChatLieuForm(ChatLieuDTO _cl)
         {
+            this.cl = _cl;
             InitializeComponent();
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void DetailChatLieuForm_Load(object sender, EventArgs e)
+        {
+            txtTenChatLieu.Text = cl.Tenchatlieu.ToString();
+            txtTenChatLieu.Enabled = false; //Chặn sửa
+        }
+
+        private void DetailChatLieuForm_Shown(object sender, EventArgs e)
+        {
+            txtTenChatLieu.SelectionLength = 0;
         }
     }
 }
