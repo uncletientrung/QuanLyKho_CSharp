490ec1bb4c3d7ae1a1b2bcf02b70bf9a48f09151
Thanh Tùng <phanthanhtung071105@gmail.com>
2025-10-10 15:51:57 +0700

    giaodien addSize

---
+++ GUI/ThongTin/Size/AddSizeForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKho_CSharp.GUI.ThongTin.Size
+{
+    public partial class AddSizeForm : Form
+    {
+        public AddSizeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
