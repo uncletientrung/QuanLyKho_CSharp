ad46f1cc238f8b463d82c98278ac811aec2236fd
thuanstealer <nmthuan645@gmail.com>
2025-10-11 16:26:53 +0700

    sua giao dien thong ke

---
+++ GUI/ThongKe/giaoDienTK/UCTongQuan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKho_CSharp.GUI.ThongKe.giaoDienTK
+{
+    public partial class UCTongQuan : UserControl
+    {
+        public UCTongQuan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinhHangTon_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
