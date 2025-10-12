4203c1e1bd3668fb8b867474772de5fdf0d8b942
thuanstealer <nmthuan645@gmail.com>
2025-10-12 10:14:44 +0700

    sua tk

--- GUI/ThongKe/giaoDienTK/UCTongQuan.Designer.cs
+++ GUI/ThongKe/giaoDienTK/UCTongQuan.Designer.cs
@@ -47,6 +47,9 @@
             this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.dgvDoanhThu7ngay = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -56,6 +59,7 @@
             this.tableLayoutPanel4.SuspendLayout();
             this.tableLayoutPanel5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhThu7ngay)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -157,10 +161,11 @@
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.tableLayoutPanel1);
             this.panel2.Controls.Add(this.tableLayoutPanel3);
             this.panel2.Location = new System.Drawing.Point(0, 100);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1043, 213);
+            this.panel2.Size = new System.Drawing.Size(1043, 170);
             this.panel2.TabIndex = 1;
             // 
             // tableLayoutPanel3
@@ -168,14 +173,14 @@
             this.tableLayoutPanel3.ColumnCount = 2;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
-            this.tableLayoutPanel3.Controls.Add(this.chartLoaiSP, 1, 0);
             this.tableLayoutPanel3.Controls.Add(this.bieuDoDoanhThuTheoNgay, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.chartLoaiSP, 1, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(1043, 213);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1043, 170);
             this.tableLayoutPanel3.TabIndex = 1;
             // 
             // chartLoaiSP
@@ -192,7 +197,7 @@
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chartLoaiSP.Series.Add(series1);
-            this.chartLoaiSP.Size = new System.Drawing.Size(360, 207);
+            this.chartLoaiSP.Size = new System.Drawing.Size(360, 164);
             this.chartLoaiSP.TabIndex = 0;
             this.chartLoaiSP.Text = "chart1";
             // 
@@ -202,7 +207,7 @@
             this.bieuDoDoanhThuTheoNgay.Dock = System.Windows.Forms.DockStyle.Fill;
             this.bieuDoDoanhThuTheoNgay.Location = new System.Drawing.Point(3, 3);
             this.bieuDoDoanhThuTheoNgay.Name = "bieuDoDoanhThuTheoNgay";
-            this.bieuDoDoanhThuTheoNgay.Size = new System.Drawing.Size(671, 207);
+            this.bieuDoDoanhThuTheoNgay.Size = new System.Drawing.Size(671, 164);
             this.bieuDoDoanhThuTheoNgay.TabIndex = 1;
             this.bieuDoDoanhThuTheoNgay.Text = "cartesianChart1";
             // 
@@ -210,9 +215,9 @@
             // 
             this.panel3.Controls.Add(this.tableLayoutPanel4);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel3.Location = new System.Drawing.Point(0, 319);
+            this.panel3.Location = new System.Drawing.Point(0, 276);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1043, 251);
+            this.panel3.Size = new System.Drawing.Size(1043, 294);
             this.panel3.TabIndex = 2;
             // 
             // tableLayoutPanel4
@@ -227,7 +232,7 @@
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
             this.tableLayoutPanel4.RowCount = 1;
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel4.Size = new System.Drawing.Size(1043, 251);
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(1043, 294);
             this.tableLayoutPanel4.TabIndex = 1;
             // 
             // tableLayoutPanel5
@@ -241,7 +246,7 @@
             this.tableLayoutPanel5.RowCount = 2;
             this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.00681F));
             this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.99319F));
-            this.tableLayoutPanel5.Size = new System.Drawing.Size(360, 245);
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(360, 288);
             this.tableLayoutPanel5.TabIndex = 1;
             // 
             // label1
@@ -251,7 +256,7 @@
             this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(354, 41);
+            this.label1.Size = new System.Drawing.Size(354, 48);
             this.label1.TabIndex = 0;
             this.label1.Text = "Top 3 xuất kho nhiều nhất";
             // 
@@ -265,8 +270,47 @@
             this.dgvDoanhThu7ngay.Name = "dgvDoanhThu7ngay";
             this.dgvDoanhThu7ngay.RowHeadersWidth = 51;
             this.dgvDoanhThu7ngay.RowTemplate.Height = 24;
-            this.dgvDoanhThu7ngay.Size = new System.Drawing.Size(671, 245);
+            this.dgvDoanhThu7ngay.Size = new System.Drawing.Size(671, 288);
             this.dgvDoanhThu7ngay.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1043, 25);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(671, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "THỐNG KÊ DOANH THU 7 NGÀY GẦN ĐÂY";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(680, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(360, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "TỈ LỆ LOẠI SẢN PHẨM TRONG KHO";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // UCTongQuan
             // 
@@ -287,6 +331,8 @@
             this.tableLayoutPanel5.ResumeLayout(false);
             this.tableLayoutPanel5.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhThu7ngay)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -308,5 +354,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgvDoanhThu7ngay;
         private LiveCharts.WinForms.CartesianChart bieuDoDoanhThuTheoNgay;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- GUI/ThongKe/giaoDienTK/UCTongQuan.cs
+++ GUI/ThongKe/giaoDienTK/UCTongQuan.cs
@@ -1,4 +1,10 @@
-﻿using QuanLyKho_CSharp.BUS;
+﻿using LiveCharts;
+using LiveCharts.WinForms;
+using LiveCharts.Wpf;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using QuanLyKho_CSharp.BUS;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,12 +15,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
-using LiveCharts;
-using LiveCharts.Wpf;
-using LiveCharts.WinForms;
-using SeriesCollection = LiveCharts.SeriesCollection;
 using Axis = LiveCharts.Wpf.Axis;
 using Series = System.Windows.Forms.DataVisualization.Charting.Series;
+using SeriesCollection = LiveCharts.SeriesCollection;
+
+
+
 
 namespace QuanLyKho_CSharp.GUI.ThongKe.giaoDienTK
 {
@@ -32,41 +38,14 @@ namespace QuanLyKho_CSharp.GUI.ThongKe.giaoDienTK
         private void UCTongQuan_Load(object sender, EventArgs e)
         {
 
-            //LoadChart();
+   
             LoadChartLiveCharts();
             //LoadTable();
             LoadTongSo();
             LoadBieuDoLoaiSP();
+           
         }
 
-
-        //private void LoadChart()
-        //{
-        //    var data = tkBUS.thongKe7NgayGanDay();
-
-        //    chartDoanhThu7ngayGanDay.Series.Clear();
-        //    chartDoanhThu7ngayGanDay.ChartAreas[0].AxisX.Title = "Ngày";
-        //    chartDoanhThu7ngayGanDay.ChartAreas[0].AxisY.Title = "Đơn vị: Triệu đồng";
-        //    chartDoanhThu7ngayGanDay.ChartAreas[0].AxisY.Interval = 2_000_000;
-        //    chartDoanhThu7ngayGanDay.ChartAreas[0].AxisY.Minimum = 0;
-        //    chartDoanhThu7ngayGanDay.ChartAreas[0].AxisY.Maximum = 20_000_000;
-
-        //    var von = new Series("Vốn") { ChartType = SeriesChartType.Column, Color = Color.SkyBlue };
-        //    var doanhThu = new Series("Doanh thu") { ChartType = SeriesChartType.Column, Color = Color.SeaGreen };
-        //    var loiNhuan = new Series("Lợi nhuận") { ChartType = SeriesChartType.Column, Color = Color.OrangeRed };
-
-        //    foreach (var item in data)
-        //    {
-        //        string label = item.Ngay.ToString("dd/MM");
-        //        von.Points.AddXY(label, item.Chiphi);
-        //        doanhThu.Points.AddXY(label, item.Doanhthu);
-        //        loiNhuan.Points.AddXY(label, item.Loinhuan);
-        //    }
-
-        //    chartDoanhThu7ngayGanDay.Series.Add(von);
-        //    chartDoanhThu7ngayGanDay.Series.Add(doanhThu);
-        //    chartDoanhThu7ngayGanDay.Series.Add(loiNhuan);
-        //}
 
         private void LoadChartLiveCharts()
         {
@@ -129,10 +108,11 @@ namespace QuanLyKho_CSharp.GUI.ThongKe.giaoDienTK
                 }
             });
 
-            bieuDoDoanhThuTheoNgay.AxisY.Add(new Axis
+            bieuDoDoanhThuTheoNgay.AxisY.Add(new LiveCharts.Wpf.Axis
             {
                 Title = "Đơn vị: Triệu đồng",
-                LabelFormatter = value => value.ToString("N0")
+                LabelFormatter = value => value.ToString("N0"),
+                MaxValue = 20 // tương ứng 20 triệu
             });
 
             bieuDoDoanhThuTheoNgay.LegendLocation = LegendLocation.Right;
@@ -223,16 +203,10 @@ namespace QuanLyKho_CSharp.GUI.ThongKe.giaoDienTK
             // Thêm series vào chart
             chartLoaiSP.Series.Add(series);
 
-            // ====== Tùy chỉnh giao diện ======
-            chartLoaiSP.Titles.Clear();
-            chartLoaiSP.Titles.Add("TỈ LỆ LOẠI SẢN PHẨM TRONG KHO");
-            chartLoaiSP.Titles[0].Font = new Font("Segoe UI", 14, FontStyle.Bold);
-            chartLoaiSP.BackColor = Color.WhiteSmoke;
-            area.BackColor = Color.White;
-
-            // Làm tròn góc và 3D nhẹ
-            area.Area3DStyle.Enable3D = true;
         }
+
+
+
         private void btnTinhHangTon_Click(object sender, EventArgs e)
         {
             
