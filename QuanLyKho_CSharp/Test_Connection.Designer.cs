2ac3f05db6aa7663fae98a315817d769026049dd
vu-tm <truongm.vuu@gmail.com>
2025-12-11 19:20:02 +0700

    doi cai anh bia

--- QuanLyKho_CSharp/Test_Connection.Designer.cs
+++ QuanLyKho_CSharp/Test_Connection.Designer.cs
@@ -45,9 +45,10 @@
             this.artanPanel1.GradientAngle = 90F;
             this.artanPanel1.GradientBottomColor = System.Drawing.Color.CadetBlue;
             this.artanPanel1.GradientTopColor = System.Drawing.Color.DodgerBlue;
-            this.artanPanel1.Location = new System.Drawing.Point(5, 5);
+            this.artanPanel1.Location = new System.Drawing.Point(7, 6);
+            this.artanPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.artanPanel1.Name = "artanPanel1";
-            this.artanPanel1.Size = new System.Drawing.Size(790, 440);
+            this.artanPanel1.Size = new System.Drawing.Size(1053, 542);
             this.artanPanel1.TabIndex = 1;
             // 
             // pictureBox1
@@ -55,21 +56,23 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(790, 440);
+            this.pictureBox1.Size = new System.Drawing.Size(1053, 542);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Test_Connection
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.artanPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Test_Connection";
-            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Test_Connection_Load);
             this.artanPanel1.ResumeLayout(false);
